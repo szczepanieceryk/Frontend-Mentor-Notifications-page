db5c06380336f8fe46920b7ea5822d4ea259c701
eryk <e.szczepaniec@lightpixel.io>
2024-01-10 14:01:09 +0100

    Get number of active notifications & update state

--- src/App.js
+++ src/App.js
@@ -7,7 +7,16 @@ import "./styles/style.css";
 import Footer from "./pages/Footer";
 
 const App = () => {
-  const [notificationCount, setNotificationCount] = useState(3);
+  // Get number of active notifications & use it to set up default state value
+  const activeNotificationCount = () => {
+    return notificationsData.filter(
+      (notification) => notification.activeClass === "active"
+    ).length;
+  };
+
+  const [notificationCount, setNotificationCount] = useState(
+    activeNotificationCount()
+  );
 
   return (
     <div className="app-wrapper">
